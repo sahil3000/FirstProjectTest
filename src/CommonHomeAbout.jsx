48cf79d4d1f0b3119c3b36e8419126ca7c0a5dfb
Sahil Manhotra <sahil0730@gmail.com>
2020-09-13 23:48:11 +0530

    First commit of  Project file

---
+++ src/CommonHomeAbout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const CommonHomeAbout = (props)=>{
+    return(
+        <>
+            <section id="heading" className="d-flex align-items-center">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="row">
+                                
+                                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                                    <h1>{props.name }<strong className="brand-name">ABC.com</strong></h1>
+                                    <h2 className="my-3">We are the team of talented developer making websites</h2>
+                                    <div className="mb-5">
+                                        <NavLink to={props.visit} className="btn btn-outline-primary">{props.btnMsg}</NavLink>
+                                    </div>
+                                </div>
+
+
+                                <div className="mt-5 pt-5 col-md-6 order-1 order-lg-2 header-img text-center">
+                                    <img src={props.imgLink} className="img-fluid animated" width="300" height="500" alt="pics"/>
+                                </div>
+
+                            </div>          
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+export default CommonHomeAbout;
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Footer = ()=>{
+    return(
+        <>
+            <footer className="pt-5 pt-5 bg-light text-center">
+                <p>
+                    © 2020 Sahil Manhotra. All Rights Reserved | Terms and Conditions
+                </p>
+            </footer>
+        </>
+    );
+}
+export default Footer;
