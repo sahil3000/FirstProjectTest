03feb24170a8a8a605a3c22281217197041a8a88
Sahil Manhotra <sahil0730@gmail.com>
2020-09-14 13:54:05 +0530

    Updates

---
+++ precache-manifest.6c78b99a9aaac400adfc0db2b8f67446.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b96d412444f757a6b8b097556005b5",
+    "url": "/firstprojecttest/index.html"
+  },
+  {
+    "revision": "bd6e082e87cf35000fbd",
+    "url": "/firstprojecttest/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "96c974260337cf5e6446",
+    "url": "/firstprojecttest/static/css/main.31b74269.chunk.css"
+  },
+  {
+    "revision": "bd6e082e87cf35000fbd",
+    "url": "/firstprojecttest/static/js/2.4a134d9e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/firstprojecttest/static/js/2.4a134d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c974260337cf5e6446",
+    "url": "/firstprojecttest/static/js/main.73d4c319.chunk.js"
+  },
+  {
+    "revision": "fb81a185709e73969f20",
+    "url": "/firstprojecttest/static/js/runtime-main.c6f2f8af.js"
+  },
+  {
+    "revision": "139767e2cf9004e4f0582407862f8313",
+    "url": "/firstprojecttest/static/media/img1.139767e2.png"
+  },
+  {
+    "revision": "e6b214caeabe5409273520215449d4fa",
+    "url": "/firstprojecttest/static/media/service1.e6b214ca.jpg"
+  },
+  {
+    "revision": "64fa3844b04d8d48ecd346d8612ccaf1",
+    "url": "/firstprojecttest/static/media/service2.64fa3844.jpg"
+  },
+  {
+    "revision": "4236c44762a99e35c6ca0b9a42960196",
+    "url": "/firstprojecttest/static/media/service3.4236c447.jpg"
+  },
+  {
+    "revision": "d56d735472726982b76e9ae15f6bcaca",
+    "url": "/firstprojecttest/static/media/service4.d56d7354.jpg"
+  },
+  {
+    "revision": "4f8d9d71f87d4e26618e10b65b59d2d2",
+    "url": "/firstprojecttest/static/media/service5.4f8d9d71.jpg"
+  },
+  {
+    "revision": "c1a162643e805f2e056fca268eba3715",
+    "url": "/firstprojecttest/static/media/service6.c1a16264.jpg"
+  }
+]);
